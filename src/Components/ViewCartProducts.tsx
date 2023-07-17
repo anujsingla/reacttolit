215141de41ca127d277f13c00a06c2917cca6307
Anuj Singla <asingla@redhat.com>
2023-07-17 11:10:39 +0530

    added cart feature .

--- src/Components/ViewCartProducts.tsx
+++ src/Components/ViewCartProducts.tsx
@@ -1,3 +1,53 @@
+import {
+  CardBody,
+  Flex,
+  FlexItem,
+  Grid,
+  GridItem,
+  List,
+  ListItem,
+} from "@patternfly/react-core";
+import { find, isEmpty, map } from "lodash";
+import { useAppSelector } from "../redux/reduxHook";
+
 export const ViewCartProducts = () => {
-  return <div>ViewCartProducts</div>;
+  const { productCart, totalPrice } = useAppSelector(
+    (state) => state.productCart
+  );
+  const { data: products } = useAppSelector((state) => state.products.products);
+  if (isEmpty(productCart)) {
+    return <div>Add items in the cart</div>;
+  }
+  return (
+    <>
+      <List isPlain isBordered>
+        {map(productCart, (c) => {
+          const productDetail = find(products, (p) => p.id === c.id);
+          return (
+            <ListItem>
+              <Grid md={2}>
+                <GridItem>
+                  <img
+                    src={productDetail?.image}
+                    alt={"Carlie Anglemire"}
+                    height="180"
+                    width="180"
+                  />
+                </GridItem>
+                <GridItem>
+                  <Flex direction={{ default: "column" }}>
+                    <FlexItem>{productDetail?.title}</FlexItem>
+                    <FlexItem>₹{productDetail?.price}</FlexItem>
+                  </Flex>
+                </GridItem>
+              </Grid>
+            </ListItem>
+          );
+        })}
+        <ListItem className="text-align-right">
+          Total Price: <b>{totalPrice}</b>
+        </ListItem>
+      </List>
+    </>
+  );
 };
