5c27f05e163b7aa517d8f28a005be5eb99cc4b0f
Benny Powers <web@bennypowers.com>
2023-07-18 10:51:20 +0300

    Refactor About component to Lit

--- src/Components/webcomponents/AboutPage.ts
+++ src/Components/webcomponents/AboutPage.ts
@@ -1,5 +1,7 @@
 import { LitElement, html, css } from "lit";
+import { customElement } from 'lit/decorators/custom-element.js'
 
+@customElement('about-page')
 export class AboutPage extends LitElement {
   static styles = css`
     /* Your component's styles here */
@@ -9,8 +11,10 @@ export class AboutPage extends LitElement {
   `;
 
   render() {
-    return html` <p>Hello from LitElement component!</p> `;
+    return html`
+      <p>
+        We are creating an example project to help understand how to migrate a
+        React project to a Lit project.
+      </p>`;
   }
 }
-
-customElements.define("about-page", AboutPage);
