d4e04cfd656441211372e8d28c9cdcdbbd85218a
Anuj Singla <asingla@redhat.com>
2023-08-26 09:05:57 +0530

    remove react from the app .

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import "@patternfly/react-core/dist/styles/base.css";
+// import "@patternfly/patternfly/patternfly.css";
+import "./css/app.css";
+
+
+import { LitElement, html, css } from "lit";
+import { customElement } from "lit/decorators/custom-element.js";
+import "./Components/webcomponents/AppLayoutPage";
+
+@customElement("main-app")
+export class MainApp extends LitElement {
+  static styles = css`
+    /* Your component's styles here */
+    :host {
+      display: block;
+    }
+  `;
+
+  render() {
+    return html` <app-layout></app-layout>`;
+  }
+}
+
